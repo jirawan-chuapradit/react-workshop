657dcb9fceee4bd9b5cd8780f9f7e19162307509
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2022-03-13 16:16:12 +0700

    create component

---
+++ quiz-app/src/components/Menu.js
@@ -0,0 +1,9 @@
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h1>Menu component</h1>
+    </div>
+  );
+};
+
+export default Menu;
---
+++ quiz-app/src/components/Quiz.js
@@ -0,0 +1,10 @@
+
+const Quiz = () => {
+    return (
+        <div className="quiz">
+            <h1>Quiz component</h1>
+        </div>
+    )
+};
+
+export default Quiz;
