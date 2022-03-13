f00f0c171dd62446ffb2831fdaf30d6d82acda0f
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2022-03-13 18:22:33 +0700

    state management

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
+import Menu from "./components/Menu";
+import Quiz from "./components/Quiz";
+import Score from "./components/Score";
 function App() {
+  const [appState, setAppState] = useState("menu");
   return (
     <div className="App">
-      {" "}
       <h1>devlepment</h1>
+      {appState === "menu" && <Menu />}
+      {appState === "quiz" && <Quiz />}
+      {appState === "score" && <Score />}
     </div>
   );
 }
