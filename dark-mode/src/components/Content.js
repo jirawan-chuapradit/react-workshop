580f5fb89ce94f7c18ac9d72e1fb9a4a28ccb42d
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2022-10-23 19:07:50 +0700

    add style content

--- dark-mode/src/components/Content.js
+++ dark-mode/src/components/Content.js
@@ -1,9 +1,12 @@
 import light from "../image/light.svg";
 import dark from "../image/dark.svg";
+import { useContext } from "react";
+import { ThemeContext } from "../App";
 
 const Content = () => {
+   const {theme} =  useContext(ThemeContext)
   return (
-    <main>
+    <main className={theme ==="dark"? "dark": "light"}>
       <div>
         <h1>KongRuksiam official</h1>
         <p>DarkMode Workshop</p>
