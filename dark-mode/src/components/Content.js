d4bebdd0f50a7c0fcb27c7f88f18887bdfc97916
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2022-10-23 16:38:11 +0700

    create dark mode

---
+++ dark-mode/src/components/Content.js
@@ -0,0 +1,16 @@
+import light from "../image/light.svg";
+import dark from "../image/dark.svg";
+
+const Content = () => {
+  return (
+    <main>
+      <div>
+        <h1>KongRuksiam official</h1>
+        <p>DarkMode Workshop</p>
+      </div>
+      <img src={light} alt="Logo" />
+    </main>
+  );
+};
+
+export default Content;
