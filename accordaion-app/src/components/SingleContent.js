90416971b1a7f5ed300aec3867e0c06e4feb4693
Jirawan Chuapradit <jirawan-chuapradit@users.noreply.github.com>
2022-10-29 15:57:28 +0700

    react icon

--- accordaion-app/src/components/SingleContent.js
+++ accordaion-app/src/components/SingleContent.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
 
 const SingleContent=({title, description})=> {
     const [showContent, setShowContent] = useState(false)
@@ -6,9 +7,11 @@ const SingleContent=({title, description})=> {
         <article className="content">
             <header>
                 <h4>{title}</h4>
-                <button className="btn" onClick={()=>setShowContent(!showContent)}>ดูข้อมูล</button>
-                {showContent && <p>{description}</p>}
+                <button className="btn" onClick={()=>setShowContent(!showContent)}>
+                    {showContent ? <AiOutlineMinus/>:<AiOutlinePlus/>}
+                </button>
             </header>
+            {showContent && <p>{description}</p>}
         </article>
     )
 }
