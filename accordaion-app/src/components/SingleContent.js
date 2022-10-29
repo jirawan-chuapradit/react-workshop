820525e89002c11086c8499ff1f966453d43a382
Jirawan Chuapradit <jirawan-chuapradit@users.noreply.github.com>
2022-10-29 15:33:54 +0700

    add show content

--- accordaion-app/src/components/SingleContent.js
+++ accordaion-app/src/components/SingleContent.js
@@ -1,10 +1,13 @@
+import { useState } from "react"
+
 const SingleContent=({title, description})=> {
+    const [showContent, setShowContent] = useState(false)
     return (
         <article className="content">
             <header>
                 <h4>{title}</h4>
-                <button className="btn">ดูข้อมูล</button>
-                <p>{description}</p>
+                <button className="btn" onClick={()=>setShowContent(!showContent)}>ดูข้อมูล</button>
+                {showContent && <p>{description}</p>}
             </header>
         </article>
     )
